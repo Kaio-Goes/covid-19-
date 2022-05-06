e3fedf8a7b8aed2e498180e267d820918afdf3dd
Kaio Goes <kaiogoes1999@gmail.com>
2022-05-06 02:22:56 -0300

    Criando componentes da aplicação

---
+++ src/components/Chart.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {View, Text} from 'react-native'
+
+export default class Chart extends Component {
+    render(){
+        return(
+            <View>
+                <Text>
+                    Chart
+                </Text>
+            </View>
+        )
+    }
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -7,6 +7,11 @@ import {
     Button, 
     ImageBackground 
 } from 'react-native';
+import Icon from '@expo/vector-icons/Ionicons';
+import {ScrollView} from 'react-native-gesture-handler';
+import Deck from '../components/Deck';
+import Cards from '../components/Cards';
+import Buttons from '../components/Buttons';
 
 export default class Home extends Component {
     render() {
