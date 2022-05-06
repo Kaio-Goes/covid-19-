197ffa1990b1fedc3213e651b2115c457f1ff19d
Kaio Goes <kaiogoes1999@gmail.com>
2022-05-06 02:06:32 -0300

    criando a telas de navegação

---
+++ src/screens/Home.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    Image, 
+    Button, 
+    ImageBackground 
+} from 'react-native';
+
+export default class Home extends Component {
+    render() {
+        return(
+            <View>
+                <Text>Home</Text>
+            </View>
+        )
+    }
+}       
